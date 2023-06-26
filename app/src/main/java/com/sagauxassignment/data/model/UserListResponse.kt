a7ed5e880247897da2e97361121717d7a90f4e44
dholakiya_pankaj <pankajdholakiya6@gmail.com>
2023-06-26 12:23:35 +0530

    Added keep annotation to prevent model class from proguard.

--- app/src/main/java/com/sagauxassignment/data/model/UserListResponse.kt
+++ app/src/main/java/com/sagauxassignment/data/model/UserListResponse.kt
@@ -1,9 +1,12 @@
 package com.sagauxassignment.data.model
 
+import androidx.annotation.Keep
+
+@Keep
 data class UserListResponse(
     val users: List<UserResponse>?
 ) {
-
+    @Keep
     data class UserResponse(
         val id: String?,
         val name: String?,
--- app/src/main/java/com/sagauxassignment/domain/Users.kt
+++ app/src/main/java/com/sagauxassignment/domain/Users.kt
@@ -1,8 +1,12 @@
 package com.sagauxassignment.domain
 
+import androidx.annotation.Keep
+
+@Keep
 data class Users(
     val usersList: List<ApiUser>?
 ) {
+    @Keep
     data class ApiUser(
         val id: String?,
         val name: String?,
